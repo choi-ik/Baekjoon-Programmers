4d02cdb4b8dcb1437fa6733ce99fde43e7f4082d
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-04-24 19:25:21 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,11 +1,14 @@
 function solution(begin, target, words) {
     let answer = 0;
-    let wordSet = new Set(words);
     
+    /* wordSet이라는 Set 객체에 words 넣어주고 target 값이 없으면 0 return */
+    let wordSet = new Set(words);
     if(wordSet.has(target) === false) return 0;
     
+    /* 방문 배열 생성*/
     let visit = new Array(words.length).fill(false);
     
+    /* words를 탐색할 BFS 함수(최소 몇 단계의 과정을 찾는지 물음 즉, 최단거리와 같은 질문) */
     function BFS(w) {
         let queue = [w];
         
@@ -16,25 +19,25 @@ function solution(begin, target, words) {
             
             for(let i=0; i<words.length; i++) {
                 if(visit[i] === true) continue;
-                let cnt = 0;
-                let targetCnt = 0;
-                visit[i] = true;
+                let cnt = 0; // 큐에서 추출한 wArr와 words의 같은 알파벳 개수
+                let targetCnt = 0; // 큐에서 추출한 wArr와 target의 같은 알파벳 개수
+                visit[i] = true; // 방문 처리
                 
                 for(let j=0; j<wArr.length; j++) {
                     if(wArr[j] === words[i][j]) cnt++;
                     if(wArr[j] === target[j]) targetCnt++;
-                }
+                };
+                
                 if(targetCnt === begin.length-1) {
-                    queue.push(target);
+                    queue.push(target); 
                 } else if(cnt === begin.length-1) {
                     queue.push(words[i]);
                     break;
-                } else {
-                    continue;
-                }
-            }
-        }
-    }
+                };
+            };
+        };
+    };
+    
     BFS(begin);
     return answer-1;
 }
