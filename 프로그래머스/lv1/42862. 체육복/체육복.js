37acfca4017e22f478931a9cd19b19b7c1995e4a
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-04-27 18:18:22 +0900

    [level 1] Title: 체육복, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -2,18 +2,20 @@ function solution(n, lost, reserve) {
     let answer = 0;
     lost.sort();
     reserve.sort();
-    // 중복 확인
+    
+    /* 중복 확인 */
     for(let i=0; i<lost.length; i++) {
         for(let j=0; j<reserve.length; j++) {
             if(lost[i] === reserve[j]) {
                 lost.splice(i, 1);
                 reserve.splice(j, 1);
-                console.log("로스트", lost, "리버스", reserve)
                 i--;
+                continue;
             }
         }
     }
-  
+    
+    /* 빌려줄수 있는 학생 확인 */
      for(let i=0; i<lost.length; i++) {
         for(let j=0; j<reserve.length; j++) {
             if(lost[i]-1 === reserve[j] || lost[i]+1 === reserve[j]) {
