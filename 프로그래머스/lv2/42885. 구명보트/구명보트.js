bfd4ffee46c075c3c834cb39bea2df4a8e69aaa8
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-05-03 13:59:32 +0900

    [level 2] Title: 구명보트, Time: 10.80 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,14 +1,16 @@
 function solution(people, limit) {
     var answer = 0;
-    const sortedPeople = people.sort((a, b) => a - b );
-    let lInd = 0;
-    let rInd = people.length - 1;
-    while ( lInd <= rInd ) {
-        
-        if ( sortedPeople[lInd] + sortedPeople[rInd] <= limit ) {
-            lInd++;
+    const sortedPeople = people.sort((a, b) => a - b); // people 정렬
+    let front = 0; // 왼쪽 끝
+    let back = people.length - 1; // 오른쪽 끝
+    
+    while ( front <= back ) {
+        /* 양쪽 끝에서부터 서로 더하여 검사하면서 limit 보다 작다면 front를 한 칸 뒤로 당겨주고
+            비교할 때마다 back은 계속 한 칸 앞으로 당겨준다 */
+        if ( sortedPeople[front] + sortedPeople[back] <= limit ) {
+            front++;
         }
-        rInd--;
+        back--;
         answer += 1;
     }
     return answer;
