13784ed74e77f7cd51c5952fa50cef89cee3c84d
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-05-08 03:33:14 +0900

    [level 3] Title: N으로 표현, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
@@ -1,11 +1,13 @@
 function solution(N, number) {
     let answer = 0;
+    /* 길이가 8인 배열을 만들고 각각의 인덱스 안에 Set 객체 생성(중복을 줄이고, 데이터를 빠르게 찾기 위함) */
     let dp = new Array(8).fill().map(() => new Set());
     
-    for(let i=0; i<8; i++) {
-        dp[i].add(Number(`${N}`.repeat(i+1)))
-        for(let j=0; j<i; j++) {
-            for(let n of dp[j]) {
+    /* 배열의 길이 만큼 순회 */
+    for(let i=0; i<dp.length; i++) {
+        dp[i].add(Number(String(N).repeat(i+1))) // 각 인덱스에 문자열 N을 합친 수를 Number로 감싸서 add
+        for(let j=0; j<i; j++) { // 각 인덱스 번호 만큼 N을 반복하기 위함
+            for(let n of dp[j]) { // ex) i가 3이라면 -> n=dp[0], m=dp[2] / n=dp[1], m=dp[1] / n=dp[2], m=dp[0] => n과 m에 1씩 더하면 더 이해하기 쉬움   
                 for(let m of dp[i-j-1]) {
                     dp[i].add(n * m);
                     dp[i].add(Math.floor(n / m));
@@ -14,9 +16,8 @@ function solution(N, number) {
                 }
             }
         }
-        if(dp[i].has(number)) return i+1;
+        if(dp[i].has(number)) return i+1; // dp[i]에 찾는 number가 있다면 그 인덱스에 1을 더하여 반환
     };
     
-    console.log(dp)
     return -1;
 }
