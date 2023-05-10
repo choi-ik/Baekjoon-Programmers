fc89740c6d97e2fec14d8bdb771e34e46d648dd4
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-05-10 22:51:19 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 39.08 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,16 +1,19 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    deliveries = deliveries[::-1] # 배달 배열 역순 정렬
-    pickups = pickups[::-1] # 픽업 배열 역순 정렬
+    deliveries = deliveries[::-1] # 배달 배열 역순 정렬.
+    pickups = pickups[::-1] # 픽업 배열 역순 정렬.
     
     giveBox = 0
     receiveBox = 0
     
-    for i in range(n):
-        giveBox += deliveries[i]
+    for i in range(n): # 역순으로 정렬된 배열 순회.
+        giveBox += deliveries[i] 
         receiveBox += pickups[i]
-        while giveBox > 0 or receiveBox > 0:
+        # giveBox와 receiveBox가 음수라면 한번에 실어나를 수 있는 양보다 적은 것이므로 오가는 길에 추가로 배달 픽업 가능.
+        # giveBox와 receiveBox가 양수라면 실어나를 수 있는 양보다 많은 것이므로 다시 들려야함.
+        while giveBox > 0 or receiveBox > 0: 
             giveBox -= cap
             receiveBox -= cap
             answer += (n - i) * 2
+            
     return answer
