c8e7aee27ba1b79cc7f595ed494f61e02918ee8d
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-05-12 20:52:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.40 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,25 +1,25 @@
 function solution(maps) {
     let answer = [];
-    
+
     // 이동 할 네 가지 방향 정의 (상->하->좌->우) 순서
     let dx = [-1, 1, 0, 0];
     let dy = [0, 0, -1, 1];
-    
+
     function BFS(x, y) {
         let queue = [[x,y]];
 
         while(queue.length > 0) {
             const [x1, y1] = queue.shift();
-            
+
             for(let i=0; i<4; i++) {
                 let nx = x1 + dx[i];
                 let ny = y1 + dy[i];
                 // 배열을 벗어날 경우 무시하기
                 if(nx < 0 || nx > maps.length-1 || ny < 0 || ny > maps[0].length-1) continue;
-                
+
                 // 벽을 만난 경우 무시
                 if(maps[nx][ny] === 0) continue;
-                
+
                 // 해당 노드를 처음 방문할 때만 최단 거리 기록
                 if(maps[nx][ny] === 1) {
                     maps[nx][ny] = maps[x1][y1] + 1;
@@ -33,5 +33,5 @@ function solution(maps) {
     if(maps[maps.length-1][maps[0].length-1] === 1) return -1;
 
     return BFS(0, 0);
-    
+
 }
