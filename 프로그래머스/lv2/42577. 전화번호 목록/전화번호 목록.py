1bcc5c19ee3de464b896669b9d8ed117e66d2573
choi-ik <91654577+choi-ik@users.noreply.github.com>
2023-05-21 02:19:21 +0900

    [level 2] Title: 전화번호 목록, Time: 97.05 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,13 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    obj = {}
+#     obj = {}
 
-    for i in range(len(phone_book)):
-        obj[i] = phone_book[i]
+#     for i in range(len(phone_book)):
+#         obj[i] = phone_book[i]
     
     for i in range(len(phone_book) - 1):
-            if obj[i] in obj[i+1][:len(obj[i])]:
+            if phone_book[i] in phone_book[i+1][:len(phone_book[i])]:
                 return False
             
     return answer
