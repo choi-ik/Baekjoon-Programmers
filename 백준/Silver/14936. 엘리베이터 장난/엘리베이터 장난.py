9abc6723bef21dacf89e0a01175f7bc7e2c85c4c
Choi-ik <ci980704@naver.com>
2023-05-29 15:33:49 +0900

    [Silver IV] Title: 엘리베이터 장난, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14936. 엘리베이터 장난/엘리베이터 장난.py
+++ 백준/Silver/14936. 엘리베이터 장난/엘리베이터 장난.py
@@ -11,25 +11,12 @@ m4 = (n - 1) // 3 + 1  # 1포함, 3k+1 버튼
 # 아무 버튼도 누르지 않은 상태
 count = 1
 
-# 모든버튼(동작 1)
 if m1 <= m: count += 1
-
-# 짝수버튼(동작 2)
-if n > 1 and m2 <= m: count += 1
-
-# 홀수버튼(동작 3)
+if n > 1 and m2 <= m: count += 1 
 if n > 1 and m3 <= m: count += 1
-
-# 3k+1버튼(동작 4)
 if n > 2 and m4 <= m: count += 1
-
-# 짝수버튼 & 3k+1버튼(동작 2 & 동작 4)
-if n >= 3 and m2 + m4 <= m: count += 1
-
-# 홀수버튼 & 3k+1버튼(동작 3 & 동작 4)
-if n >= 3 and m3 + m4 <= m: count += 1
-
-# 모든버튼 & 3k+1버튼(동작 1 & 동작 4)
-if n >= 3 and m1 + m4 <= m: count += 1
-
+if n > 2 and m2 + m4 <= m: count += 1
+if n > 2 and m3 + m4 <= m: count += 1
+if n > 2 and m1 + m4 <= m: count += 1
+    
 print(count)
