af4149d761738f0a9c7637a8baeedc6cfced45e9
Choi-ik <ci980704@naver.com>
2023-06-08 02:25:22 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 180 ms, Memory: 9896 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,41 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const N = +input[0];
+let seq = input[1].split(" ").map(value => +value);
+let lis = [];
+
+// 이분 탐색 함수
+const binarySearch = (left, right, target) => {
+    let mid = 0;
+
+    while (left < right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (lis[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return right;
+}
+
+let i = 1;
+let j = 0;
+lis[0] = seq[0];
+
+while (i < N) {
+    if (lis[j] < seq[i]) {
+        lis[j + 1] = seq[i];
+        j++;
+    } else {
+        let idx = binarySearch(0, j, seq[i]);
+        lis[idx] = seq[i];
+    }
+    
+    i++;
+}
+
+console.log(j + 1);
