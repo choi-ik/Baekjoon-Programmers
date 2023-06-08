1fa994410f577715c570d989d9cbcc0b22d8daa6
Choi-ik <ci980704@naver.com>
2023-06-08 17:57:04 +0900

    [Silver II] Title: 민균이의 계략, Time: 204 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.js
@@ -0,0 +1,39 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const N = +input[0];
+let seq = input[1].split(" ").map(value => +value);
+let lis = [seq[0]];
+
+const binarySearch = (left, right, target) => {
+    let mid = 0;
+
+    while(left < right) {
+        mid = Math.floor((left + right) / 2);
+            
+        if (lis[mid] < target) {
+            left = mid + 1;
+        } else {
+            right= mid;
+        }
+    }
+    
+    return right;
+ }
+
+ let i = 1;
+ let j = 0;
+
+ while(i < N) {
+    if (lis[j] < seq[i]) {
+        lis[j + 1] = seq[i];
+        j++;
+    } else {
+        let idx = binarySearch(0, j, seq[i]);
+        lis[idx] = seq[i];
+    }
+
+    i++;
+ }
+
+console.log(lis.length)
