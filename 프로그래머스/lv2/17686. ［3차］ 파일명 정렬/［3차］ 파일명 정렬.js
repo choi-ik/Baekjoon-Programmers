db62ed35109f6651e3104951d1f2719cf7d62fa3
Choi-ik <ci980704@naver.com>
2023-07-10 13:25:32 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 14.66 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -13,7 +13,9 @@ function solution(files) {
         
         // head가 0이 아니라면 두 크기에 따라 오름차순 또는 내림차순 정렬 return
         if (head) return head;
+        // head가 같을 때, 두 숫자를 뺀 값이 0이라면 0을 리턴해 들어온 순서 그대로 정렬
         if (Number(preNumber) - Number(curNumber) === 0) return 0;
+        // 두 숫자가 같지않을 때, 두 수를 뺀 값을 이용해 정렬
         return Number(preNumber) - Number(curNumber);
     });
     
