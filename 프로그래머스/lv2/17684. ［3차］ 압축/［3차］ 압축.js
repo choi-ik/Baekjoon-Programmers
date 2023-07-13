dfb89e05ac03bceef12da1e8c0876a3929e220fc
Choi-ik <ci980704@naver.com>
2023-07-14 02:42:44 +0900

    [level 2] Title: [3차] 압축, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -8,25 +8,25 @@ function solution(msg) {
     }
     
     let idx = 1;
-    let str = msg[0];
+    let str = msg[0]; // 현재 입력
     
+    // msg 전체를 순회하며 사전에 찾는 문자가 있는지 없는지 검사
     while (idx <= msg.length) {
-        let w = str + msg[idx];
+        let w = str + msg[idx]; // 현재 입력과 다음 글자 합친 문자
         
         // 현재 입력과 다음 글자를 합친 글자가 사전에 존재하지 않을 때
         if (!alpha.includes(w)) {
-            console.log(w)
             answer.push(alpha.indexOf(str)); // 현재 입력의 사전 번호
             alpha.push(w) // 사전에 새로운 문자열 추가
-            str = msg[idx];
+            str = msg[idx]; // 현재 입력 다음 문자로 초기화
             idx ++
         }
          // 현재 입력과 다음 글자를 합친 글자가 사전에 존재할 때
         else {
             str += msg[idx];
             idx ++;
-        } 
-    }
-    
+        };
+    };
+   
     return answer;
 }
