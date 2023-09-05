571a3f351108ee10b09c8fe1fcbe07ddfe8aa7e4
Choi-ik <ci980704@naver.com>
2023-09-05 13:13:23 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,11 +1,14 @@
 function solution(N, stages) {
     var answer = [];
-    for(var i=1; i<=N; i++){
-        var stage = stages.filter((x) => x >= i); //stages의 원소가 i보다 크거나 같은것 들은 stage에 대입
-        var fault = stages.filter((x) => x == i); //stages의 원소가 i랑 같은것 들은 fault에 대입
-        answer.push([i, fault.length/stage.length]); //answer에 fault의 길이를 stage의 길이만큼 나누고 2차월 배열로 push
-    }
-    answer.sort((a,b) => b[1] - a[1]); // sort를 이용해 a,b원소의 두번째 index인 b[1]-a[1]를 비교해 내림차순 정렬
-    return answer.map((a) => a[0]); // map함수를 이용해 이차원 배열의 원소중 첫번째 인덱스만 return
-
+    
+    for (let i = 1; i <= N; i ++) {
+        let size = stages.filter(e => e >= i).length;
+        let fail = stages.filter(e => e === i).length;
+        
+        answer.push([i, fail / size]);
+    };
+    
+    answer.sort((a, b) => b[1] - a[1]);
+    answer = answer.map(e => e[0]);
+    return answer;
 }
