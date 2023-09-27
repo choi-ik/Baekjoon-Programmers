238ebf0f9a1603c55223dfff7987b6a6d7e2c9d7
Choi-ik <ci980704@naver.com>
2023-09-27 23:41:41 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -2,7 +2,9 @@ function solution(n, computers) {
     let answer = 0;
     let visit = Array.from({length: computers.length}, () => true); // 방문 배열
     
+    // 각각의 컴퓨터를 순회하며 방문이 가능하다면 방문 가능한 컴퓨터부터 dfs 탐색을 통해 네트워크 개수 구함
     for (let i = 0; i < computers.length; i ++) {
+        // 현재 컴퓨터가 방문할 수 있다면 dfs를 실행 후 반환 받은 방문 배열을 visit에 대입
         if (visit[i]) {
             visit = dfs(i, computers, visit);
             answer ++;
@@ -10,10 +12,11 @@ function solution(n, computers) {
     }
     
     return answer;
-}
+};
 
+// dfs 탐색
 function dfs(i, computers, visit) {
-    
+    // 방문한 컴퓨터를 방문 처리 해주고, 재귀를 통해 변경한 visit 배열을 리턴하여 visit 배열에 넣은 후 리턴
     for (let idx = 0; idx < computers[i].length; idx ++) {
         if (visit[idx] && computers[i][idx] === 1) {
             visit[idx] = false;
