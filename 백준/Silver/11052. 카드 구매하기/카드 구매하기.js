414c095196578671480bd81ed5d8ee2046edb5b7
Choi-ik <ci980704@naver.com>
2024-02-06 23:49:24 +0900

    [Silver I] Title: 카드 구매하기, Time: 188 ms, Memory: 9668 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -1,16 +1,19 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "./BackJoon/11052.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const N = +input.shift();
-const card = [0, ...input.shift().split(" ").map(e => +e)];
-let dp = Array.from({length: N + 1}, () => 0);
+const card = input[0].split(" ").map(Number);
+const dp = Array(N + 1).fill(0);
+const Pi = Array(N + 1).fill(0);
 
+for (let i = 0; i < N; i++) Pi[i + 1] = card[i];
 
-for (let i = 1; i < card.length; i ++) {
-    for (let j = 1; j <= i; j ++) {
-        // 1 ~ n장의 카드를 뽑을 때 뽑을 수 있는 경우의 수를 모두 구해 그 중 가장 큰 값을 dp에 넣어 dp[N]은 가장 큰 값이 들어가게 한다.
-        dp[i] = Math.max(dp[i], dp[i-j] + card[j])
-    };
-};
+for (let i = 0; i <= N; i++) {
+  for (let j = 0; j <= i; j++) {
+    dp[i] = Math.max(dp[i], dp[i - j] + Pi[j]);
+  }
+}
 
-console.log(dp[N])
+console.log(dp[dp.length - 1]);
