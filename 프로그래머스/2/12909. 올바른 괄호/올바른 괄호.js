f7d63a38f75326e90a8da138ffb963844d1965a7
Ik <ci980704@gmail.com>
2025-01-06 01:39:54 +0900

    [level 2] Title: 올바른 괄호, Time: 33.99 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let stack = [];
+    let count = 0;
+   
+    for (let i = 0; i < s.length; i ++) {
+        if (s[i] === '(') stack.push(s[i]);
+        else {
+            if (stack.at(-1) === '(') stack.pop();
+            else stack.push(s[i]);
+        }
+    }
+    
+    return stack.length ? false : true
+}
